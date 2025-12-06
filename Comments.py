7a3461b55a68475590e35ab9de431fc5db1b3bd5
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-06 18:15:54 +0530

    Added file Comments.py

---
+++ Comments.py
@@ -0,0 +1,6 @@
+#This is a single line comment in Python language 
+print("My first program with python")
+''' This is Multiline Comment in python
+     instead of using this we can use Single line comment 
+'''
+print("End of program")
