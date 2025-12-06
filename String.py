3e3805d43411e821ae5f0a0187962cfd563ebda8
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-06 18:15:55 +0530

    Added file String.py

---
+++ String.py
@@ -0,0 +1,11 @@
+#Different ways of representing string using " ",'',''' '''
+name="Simran Ramgarhia"
+name1='Simran Ramgarhia'
+name3='''Simran Ramgarhia'''
+#String slicing
+print(name[0:7])
+print(name[1:7])
+print(name[0:7:2])
+print(name3[0:7])
+print(name1[1:7])
+print(name[0:7:2])
