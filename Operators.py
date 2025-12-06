de00775ffb66cd02b8d6302a95ff65cd29956df3
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-06 18:15:55 +0530

    Added file Operators.py

---
+++ Operators.py
@@ -0,0 +1,41 @@
+#Arithmertic operator:+,-,*,/,%
+a=8
+b=2
+c=a+b
+d=a%b
+print("The sum is:",c)
+print("The remainder is:",d)
+
+#Asiignment operator:=,+=,-+
+s=4
+s+=3
+print("The value of s is: ",s)
+
+#Comparison operator:==,>=,<=,!=
+e=6>=9
+print(e)
+
+f=8<=11
+print(f)
+
+#Logical operator
+#OR
+print("True or False is ",True or False)
+print("True or True  is ",True or True)
+print("False or False is ",False or False)
+print("False or True  is ",False or True)
+
+#And
+print("True and False is ",True and False)
+print("True and True  is ",True and True)
+print("False and False is ",False and False)
+print("False and True  is ",False and True)
+
+#NOT
+print(not(True))
+print(not(False))
+
+#type() of a variable
+z="Simran"
+print(type(a))
+print(type(z))
