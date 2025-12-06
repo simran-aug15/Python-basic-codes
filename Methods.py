c3872dd9d222f7a454c6344202f767800427bef5
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-06 18:15:54 +0530

    Added file Methods.py

---
+++ Methods.py
@@ -0,0 +1,8 @@
+name="simran Ramgarhia"
+print(len(name))
+print(name.startswith("S"))
+print(name.endswith("ia"))
+print(name.capitalize())
+a=name.find("Inderpreet")
+print(a)
+print(name.replace("Simran","Sohan"))
