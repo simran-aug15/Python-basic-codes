c154556b167f623ef3505f1e843fb411db66ba79
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 09:05:57 +0530

    Some changes

--- MethodinList.py
+++ MethodinList.py
@@ -5,3 +5,7 @@ print("The list after append is ",List.append(15))
 print("list after insertion of element at index 3 is: ",List.insert(3,30))
 print("List after popping an element at index 2 is: ",List.pop(2))
 print("list after removing an element is: ",List.remove(8))
+print("Search an element 10 in list: ",List.count(56))
+
+for list in range(1,10):
+print(List[list])
