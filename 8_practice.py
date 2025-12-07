e0eeabb8ae331ddabef6a954c516b5677ec8f916
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 10:17:22 +0530

    Some changes

---
+++ 8_practice.py
@@ -0,0 +1,8 @@
+#create an empty dictionary .allow 4 friends to enter their fav. language as value and key
+d={}
+for i in range(5):
+     name=input("Enter the name: ")
+     lang=int(input("Enter the language: "))
+     d.update({name:lang})
+
+ print(d)    
