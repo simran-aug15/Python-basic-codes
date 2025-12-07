e48af4ab343dfc33a4a808fb0560df376a608221
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 12:05:03 +0530

    Some changes

--- 19_practice.py
+++ 19_practice.py
@@ -7,9 +7,9 @@ for i in range(1,n+1):
     if(i==1 or i==n):
      print("*"*n)
 
-   else:
-   print("*",end=" ")
-   print(" "*(n-2),end=" ")
-   print("*",end=" ")
-   print(" ")     
+    else:
+     print("*",end=" ")
+     print(" "*(n-2),end=" ")
+     print("*",end=" ")
+     print(" ")     
 
