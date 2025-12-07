d5d522b4f026e022110795a3abb692989bccc3e2
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 09:02:22 +0530

    Some changes

---
+++ MethodinList.py
@@ -0,0 +1,7 @@
+List=[1,23,56,8,9,10,56,89,45]
+print("The sorted list is: ",List.sort())
+print("The reverse of alist is ",List.reverse())
+print("The list after append is ",List.append(15))
+print("list after insertion of element at index 3 is: ",List.insert(3,30))
+print("List after popping an element at index 2 is: ",List.pop(2))
+print("list after removing an element is: ",List.remove(8))
