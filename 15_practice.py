93279f112720eab6947160903b60755d1225f39a
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 11:15:27 +0530

    Some changes

---
+++ 15_practice.py
@@ -0,0 +1,5 @@
+#Write a program to greet all the person name started with S in the list
+l=["simran","samar","priya","harry","prince"]
+for name in l:
+    if(name.startswith("s")):
+        print("Hello",name)
