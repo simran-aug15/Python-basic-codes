e13a6f700b8f66b4febd8b2b07f9410535cb7993
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 10:56:53 +0530

    Some changes

---
+++ 12_practice.py
@@ -0,0 +1,7 @@
+#write a program which finds out whether a given name is present in a list for not
+l=["simran",'samar',"Raman","harry",'priya']
+name=input("Enter the name: ")
+if(name in l):
+    print("your name in l ")
+else:
+    print("Not in a l")    
