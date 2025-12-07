06c7153bd9ba08c0d989ef71a71211eb6b3fa793
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 08:55:20 +0530

    Some changes

---
+++ List.py
@@ -0,0 +1,7 @@
+friends=["Apple","Banana","Rohan",5,567,False]
+print(friends[0])
+#List is mutable and allow change value at an index
+friends[0]="grapes"
+print(friends[0])
+print(friends[0:5])
+print(friends[-5:-1])
