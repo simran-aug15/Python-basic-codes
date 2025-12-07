51d8763b1f0ddc7813ea5ed20537073b3383cb6c
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 10:51:57 +0530

    Some changes

--- 11_practice.py
+++ 11_practice.py
@@ -1,7 +1,7 @@
 #A spam comment is defined as a text containing following keywords:"Make a lot of money","buy now".write a program to detect spam
 p1="Male a lot of money"
 p2="Buy now"
-message=input("Enter the ,message ")
+message=input("Enter the message ")
 if((p1 in message) or (p2 in message)):
     print("spam")
 else:
