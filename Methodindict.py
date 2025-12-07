b008cb11ae96df2a08e5b3a32f449b1a9c0022f7
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 09:46:52 +0530

    Some changes

---
+++ Methodindict.py
@@ -0,0 +1,13 @@
+marks={"Simran":100,
+       "Samar":56,
+       "Raman":13,
+       "Akash":89}
+print("The items are: ",marks.items())
+print("The keys are: ",marks.keys())
+print,"The value are: ",(marks.value())
+a=marks.update({"Simran":98})
+print("Marks after updating is: ",a)
+b=marks.pop("Raman","13")
+print("Marks after popping is: ",b)
+c=marks.popitem()
+print("marks after pop is: ",c)
