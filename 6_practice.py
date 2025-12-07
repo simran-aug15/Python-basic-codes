0ff6a2fa53b6304b6ff93c048cdd5ab62c6e9d85
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 10:06:18 +0530

    Some changes

---
+++ 6_practice.py
@@ -0,0 +1,6 @@
+#Write a program to input eight no from the user and display it
+s=set()
+for i in range(9):
+     n = int(input("Enter the number: "))
+     s.append(n)
+     print("The numbers are: ",s)
