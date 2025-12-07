50713919bea7ad83f7dfc841310b57e283262b00
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-07 11:45:40 +0530

    Some changes

---
+++ 18-practice.py
@@ -0,0 +1,9 @@
+#Write a program to print following star pattern    *
+#                                                   **
+#                                                   ***
+
+i=1
+n=int(input("Enter the number: "))
+for i in range(i,n+1):
+    print("*"*i,end=" ")
+    print(" ")
