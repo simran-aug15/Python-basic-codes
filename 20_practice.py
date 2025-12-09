e75abff76311478c6a48c04afa91d3b4cbb337d2
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-09 19:39:05 +0530

    Some changes

--- 20_practice.py
+++ 20_practice.py
@@ -1,1 +1,14 @@
 #Write a program using function to find greatest of three number
+a=1
+b=2
+c=3
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+
+print(greatest(a,b,c))    
+    
