8362f1d1af7bae3283c0d9424d1f8cfedf12c071
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-09 22:17:00 +0530

    Some changes

---
+++ 23_practice.py
@@ -0,0 +1,9 @@
+#Write a python function to remove a given word from a list and strip at the same time
+l=["Harry","Simran","Rohan","an"]
+def rem(l,word):
+     n=[]
+     for item in l:
+          if not(item==word)
+          n.append(item.strip(word))
+          return n
+     print(rem(l,"an"))
