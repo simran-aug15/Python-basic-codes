082f8584b2f202c1d30bf12a17baa74a848c3f2b
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-09 19:33:04 +0530

    Some changes

---
+++ Function.py
@@ -0,0 +1,11 @@
+def goodDay(name):
+    print("Good Day ",name)
+goodDay("Simran")
+
+def goodDay(name,ending):
+    print("Good Day ",name)
+    print(ending)
+    return 7
+
+goodDay("simran","Thank you")
+print(a)
