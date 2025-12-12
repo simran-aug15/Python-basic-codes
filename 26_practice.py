dc616ea1a855e6be497204756161ac83ec9819c6
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-12 19:55:59 +0530

    Some changes

---
+++ 26_practice.py
@@ -0,0 +1,10 @@
+#Write a program to generate multiplication table from 2 to 12 and write it to diff files.
+def generatetable(n):
+    table=" "
+for i in range (1,11):
+    table+=f"{n} X {i}-{n*i}\n"
+with open(f"table/table-{n}","w")as f:
+    f.write(table)
+
+for i in range(2,12):
+    generatetable(i)        
