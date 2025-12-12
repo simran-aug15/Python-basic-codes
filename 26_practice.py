d980ec0773a6c2c5c82ce4d34a5bb39b43aecec9
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-12-12 19:57:36 +0530

    Some changes

--- 26_practice.py
+++ 26_practice.py
@@ -3,8 +3,8 @@ def generatetable(n):
     table=" "
 for i in range (1,11):
     table+=f"{n} X {i}-{n*i}\n"
-with open(f"table/table-{n}","w")as f:
+with open(f"table/table-{n}.txt","w")as f:
     f.write(table)
 
-for i in range(2,12):
+for i in range(2,13):
     generatetable(i)        
