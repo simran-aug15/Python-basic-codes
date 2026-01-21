ca602a189fa752e3c6a3cb0d2092875ac5cc282a
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-01-21 11:31:06 +0530

    Auto commit

--- 1_practice.py
+++ 1_practice.py
@@ -3,3 +3,6 @@ name=input("Enter the name ")
 print(f"Good afternoon {name}")
 
 
+
+
+
--- 7_practice.py
+++ 7_practice.py
@@ -5,3 +5,5 @@ s.add(18.0)#same as integer in python
 s.add(18.9)#floating number
 s.add("18")
 print("The length is: ",len(s))
+
+
